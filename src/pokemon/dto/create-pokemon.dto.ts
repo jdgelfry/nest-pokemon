1c8dc87910bae34b77792f5923dd1d9d9fdb5c58
Juan David Piedrahita <juand.piedrahitar@gmail.com>
2024-01-10 14:46:51 -0500

    conection db

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,7 @@
+export class CreatePokemonDto {
+    //isInt, isPositive, min 1
+    no: number;
+
+    //isString, isAlpha, isNotEmpty
+    name: string;
+}
---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document{
+    // id: number; //mongo lo crea automaticamente
+
+    // se pone el decorador @Prop para indicar que es un campo de la entidad, indicar que debe ser unico y que debe tener un indice
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: string;
+}
+
+// se crea el esquema de la entidad en base a la clase Pokemon en la base de datos
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
