de970e77fb85b34707823d9b2b3e33a6eca5478f
Juan David Piedrahita <juand.piedrahitar@gmail.com>
2024-02-20 21:42:46 -0500

    add configuration env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,25 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfig } from './config/env.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfig],
+    }), //carga el archivo .env por eso debe ir al inicio y se carga en el load el archivo env.config.ts para la validacion de las variables de entorno
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONDODB),
     PokemonModule,
     CommonModule,
     SeedModule
   ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log(process.env);
+  }
+}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -9,15 +9,19 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
-import internal from 'stream';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
+  private defaultLimit: number;
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) {}
+    private readonly configservice: ConfigService,
+  ) {
+    this.defaultLimit = this.configservice.get<number>('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toUpperCase(); //por la primera letra en mayuscula
@@ -30,10 +34,9 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit=this.defaultLimit, offset = 0 } = paginationDto;
     return this.pokemonModel.find()
-      .limit(limit)
-      .skip(offset)
+      .skip(limit)
       .sort({ no: 1 })
       .select('-__v');
   }
